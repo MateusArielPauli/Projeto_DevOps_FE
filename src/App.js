81a8ac8e78275aeb31dd468ed51db313b87f68f2
Mateus Ariel Pauli <62779446+MateusArielPauli@users.noreply.github.com>
2023-10-20 14:27:20 -0300

    UserList_and_UserForm

--- src/App.js
+++ src/App.js
@@ -1,52 +1,42 @@
-import logo from "./logo.svg";
-import "./App.css";
-import MeuBotao from "./components/MeuBotao";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import AppRoutes from "./AppRoutes";
+import logo from './logo.svg';
+import './App.css';
+import MeuBotao from './components/MeuBotao';
+import { BrowserRouter as Router, Link, BrowserRouter } from 'react-router-dom';
+import AppRoutes from './AppRoutes';
 
-function Header(){
+function Header() {
   return(
-    <>
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container-fluid">
-        <Link className="navbar-brand" href="/">Turismo</Link>
+        <Link to="/" className='navbar-brand'>Turismo</Link>
+        
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">Home</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Link</a>
-            </li>
-            <li className="nav-item">
-              <Link to="/user" className="nav-link"> Usuário</Link>
-            </li>
-
-            <li className="nav-item">
-              <Link to="/X" className="nav-link"> Outro </Link>
+              <Link to="/user" className='nav-link'>Usuário</Link>
             </li>
           </ul>
+
         </div>
       </div>
     </nav>
-    </>
   );
 }
 
-function App() {
+export default function App() {
   return (
     <>
-      
 
       <BrowserRouter>
-        <Header/>
+        <Header />
+
         <AppRoutes />
       </BrowserRouter>
+    
     </>
   );
 }
-
-export default App;
--- src/views/user/UserList.js
+++ src/views/user/UserList.js
@@ -1,53 +1,133 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { API_URL } from "../../AppConsts";
+import { API_URL } from '../../AppConsts';
+import { useNavigate } from "react-router-dom";
 
+/**
+ * Componente funcional de paginação dos dados.
+ *
+ * @param {*} param0
+ * @returns
+ */
+const Pagination = ({ pages, changeActivePage, changePerPage, activePage }) => {
+  const listItems = [];
+
+
+  for (let i = 0; i < pages; i++)
+    listItems.push(
+      <li className={'page-item ' + (activePage === i + 1 ? 'active' : '')} key={i}>
+        <button type="button" className="page-link" onClick={() => changeActivePage(i + 1)} >
+          {i + 1}
+        </button>
+      </li>
+    );
+
+
+  const perPage = [5, 10, 15].map(e => (
+    <option key={e} value={e}>{e}</option>
+  ));
+
+
+  return (
+    <nav>
+      <ul className="pagination justify-content-center">
+        {listItems}
+
+
+        <li>
+          <select className="form-select ms-2" onChange={changePerPage}>{perPage}</select>
+        </li>
+      </ul>
+    </nav>);
+}
+
+
+/**
+ * Componente funcional para apresentação da lista de usuários.
+ * @returns
+ */
 export default function UserList() {
-  const [users, setUsers] = useState([]);
+  const [activePage, setActivePage] = useState(1); // Estado: gerenciar a página ativa
+  const [perPage, setPerPage] = useState(5); // Estado: gerenciar a quantidade de resultados por página
+  const [data, setData] = useState({} | []); // Estado: armazenar os dados obtidos da API
+  const nav = useNavigate(); // Navegação
 
+
+
+
+  /*
+    Effect que faz a invocação da API para receber os dados dos usuários
+    em formato JSON.
+   
+    O effect observa a alteração de estado de 'activePage' e 'perPage', quando
+    algum destes estados muda, o effect é executado novamente.
+  */
   useEffect(() => {
-    axios
-      .get(API_URL + "/users")
-      .then((res) => {
-        console.log(res);
-        if (res.status == 200) {
-          setUsers(res.data.data);
-        }
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-  }, []);
+    fetch(`${API_URL}/users?page=${activePage}&per_page=${perPage}`)
+      .then(response => response.json())
+      .then(data => setData(data));
+  }, [activePage, perPage]);
+
+
+
+
+  /**
+   * Função para alterar o valor (estado) da página ativa.
+   *
+   * @param {Number} page
+   */
+  function changeActivePage(page = 1) {
+    setActivePage(page);
+  }
+
+
+  /**
+   * Função para alterar o valor (estado) da quantidade de resultados por página.
+   *
+   * @param {Number} event
+   */
+  function changePerPage(event) {
+    setPerPage(event.target.value);
+  }
+
+
+  /**
+   * Variável (array) que armazena as linhas para mostrar na tabela de usuários.
+   */
+  let userItems = data.data?.map(item => (
+    <tr key={item.id}>
+      <td><img className='avatar' src={item.avatar} alt={item.fisrt_name} /></td>
+      <td>{item.first_name} {item.last_name}</td>
+      <td>{item.email}</td>
+    </tr>
+  ));
+
 
   return (
     <>
-      <h2> Usuário </h2>
+      <h2>
+        Usuários
+        <button className="btn btn-primary ms-2" onClick={() => nav('/user/new')}>
+          <i className="fas fa-plus-circle"></i> New
+        </button>
+      </h2>
+      <p>Lista de usuários obtidos a partir da API https://reqres.in.</p>
 
-      <table className="table">
+
+      <table className='table table-striped'>
         <thead>
-          <tr>
-            <th></th>
-            <th>Nome</th>
-            <th>Email</th>
-          </tr>
+          <tr><th></th><th>Nome</th><th>E-mail</th></tr>
         </thead>
-
         <tbody>
-          {users.map((u) => (
-            <tr>
-              <td>
-                <img src={u.avatar} />
-              </td>
-              <td>
-                {u.first_name} {u.last_name}
-              </td>
-              <td>
-                {u.email} {}
-              </td>
-            </tr>
-          ))}
+          {userItems}
         </tbody>
       </table>
+
+
+      <Pagination pages={data.total_pages}
+        changeActivePage={changeActivePage}
+        changePerPage={changePerPage}
+        activePage={activePage} />
     </>
   );
 }
